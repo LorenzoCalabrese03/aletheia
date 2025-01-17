f362710a12cf8ac5657fa32168cf98d5d6468bb8
LorenzoCalabrese03 <151526103+LorenzoCalabrese03@users.noreply.github.com>
2025-01-17 16:45:40 +0100

    gg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
 
+};
+module.exports = {
+  assetPrefix: '/aletheia/',
+  basePath: '/aletheia',
+};
 export default nextConfig;
